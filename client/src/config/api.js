ae16f8fd4a49366bc5f7194506fd04a55b3eb00a
Divyanshu tandon <divyanshutandon@Divyanshus-Air.lan>
2024-01-22 22:20:52 +0530

    Initial Commit

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 
 
-// const Url = 'https://blazesolu.onrender.com';
+const Url = 'https://blazesolu.onrender.com';
 
-const Url = 'http://localhost:8000'; 
+// const Url = 'http://localhost:8000'; 
 
 export default Url;
